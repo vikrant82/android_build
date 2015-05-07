f904d9591fc797264d7811297b136992a0b61b9e
jgcaap <jgcaap@gmail.com>
2015-05-07 08:42:55 -0400

    Basic flags to build Rom New Optimizations Build: Allow board to specify additional flags Build kernel 5.1 NEST Optimize GCC only Krait Optimizations Strict Aliasing O3 Optimizations Webchromium

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -49,7 +49,7 @@ $(combo_var_prefix)HAVE_STRLCPY := 0
 $(combo_var_prefix)HAVE_STRLCAT := 0
 $(combo_var_prefix)HAVE_KERNEL_MODULES := 0
 
-$(combo_var_prefix)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar
+$(combo_var_prefix)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar  -fno-strict-aliasing
 $(combo_var_prefix)RELEASE_CFLAGS := -O2 -g -fno-strict-aliasing
 $(combo_var_prefix)GLOBAL_CPPFLAGS :=
 $(combo_var_prefix)GLOBAL_LDFLAGS :=
